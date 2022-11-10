474a64640a7d2574aadd42fa904beed19c3dedd5
mariacnm <mariacnm2@al.insper.edu.br>
2022-11-09 22:20:40 -0300

    questao de texto arrumada

--- questao_texto.py
+++ questao_texto.py
@@ -2,20 +2,10 @@ from threading import main_thread
 
 
 def questao_para_texto(dicionario_questao,id):
-    questao="----------------------------------------\n"
-    questao+='QUESTAO {0} \n'.format(id)
-    questao+="\n"
-    questao+="{0} \n".format(dicionario_questao["titulo"])
-    questao+="\n"
-    questao+='RESPOSTAS:\n'
-    questao+="A:"" "+dicionario_questao['opcoes']["A"]
-    questao+="\n"
-    questao+="B:"" "+dicionario_questao['opcoes']["B"]
-    questao+="\n"
-    questao+="C:"" "+dicionario_questao['opcoes']["C"]
-    questao+="\n"
-    questao+="D:"" "+dicionario_questao['opcoes']["D"]
-    return questao
+        return '----------------------------------------\nQUESTAO {}\n\n{}\n\nRESPOSTAS:\nA: {}\nB: {}\nC: {}\nD: {}'.format(id, dicionario_questao['titulo'], dicionario_questao['opcoes']['A'], dicionario_questao['opcoes']['B'], dicionario_questao['opcoes']['C'], dicionario_questao['opcoes']['D'])
+
+
+
 
 
 dicionario_questao={
