6f5cda6956e694bc7ed0827801d9a5f9d5192092
mariacnm <mariacnm2@al.insper.edu.br>
2022-11-11 11:37:58 -0300

    funçoes atualizadas

--- funcoes.py
+++ funcoes.py
@@ -1,47 +1,3 @@
-def valida_questao(questao):
-
-    lista1 = ['A', 'B', 'C', 'D']
-    lista2 = ['facil', 'medio', 'dificil']
-    dic = {}
-    saida_opcoes = {}
-
-    if len(questao.keys()) != 4: 
-        dic['outro'] = 'numero_chaves_invalido' 
-
-    if 'titulo' not in questao.keys(): 
-        dic['titulo'] = 'nao_encontrado'
-    else:
-        if questao['titulo'].strip() == '':
-            dic['titulo'] = 'vazio' 
-
-    if 'nivel' not in questao.keys(): 
-        dic['nivel'] = 'nao_encontrado'
-    else:
-        if questao['nivel'] not in lista2: 
-            dic['nivel'] = 'valor_errado' 
-
-    if 'opcoes' not in questao.keys(): 
-        dic['opcoes'] = 'nao_encontrado'
-    else:
-        if len(questao['opcoes'].keys()) != 4: 
-            dic['opcoes'] = 'tamanho_invalido' 
-        else:
-            for a,b in questao['opcoes'].items():
-                if a in lista1:
-                    if b.strip() == '':
-                        saida_opcoes[a] = 'vazia'
-                        dic['opcoes'] = saida_opcoes
-                else:
-                    saida_opcoes[a] = 'chave_invalida_ou_nao_encontrada'
-                    dic['opcoes'] = saida_opcoes 
-
-    if 'correta' not in questao.keys():
-        dic['correta'] = 'nao_encontrado'
-    else: 
-        if questao['correta'] not in lista1:
-            dic['correta'] = 'valor_errado'
-
-    return dic
 
 
 def transforma_base(questoes):
@@ -77,7 +33,6 @@ def sorteia_questao_inedida(dicionario_questao,nivel,lista_questao):
     dicionario_questao[nivel]
 
 
-from threading import main_thread
 
 
 def questao_para_texto(dicionario_questao,id):
@@ -105,3 +60,60 @@ def gera_ajuda(questao):
         else:
             return ""
     return resposta
+
+
+
+
+def valida_questoes (lista_questoes):
+    def valida_questao (questao):
+
+        saida = {}
+        saida_opcoes = {}
+        lista_letras = ['A', 'B', 'C', 'D']
+        lista_niveis = ['facil', 'medio', 'dificil']
+
+        if len(questao.keys()) != 4: 
+           saida['outro'] = 'numero_chaves_invalido' #2
+
+        if 'titulo' not in questao.keys(): #1
+            saida['titulo'] = 'nao_encontrado'
+        else:
+        
+            if questao['titulo'].strip() == '':
+                saida['titulo'] = 'vazio' #3
+
+        if 'nivel' not in questao.keys(): #1
+            saida['nivel'] = 'nao_encontrado'
+        else:
+            if questao['nivel'] not in lista_niveis: 
+                saida['nivel'] = 'valor_errado' #4
+
+        if 'opcoes' not in questao.keys(): #1
+            saida['opcoes'] = 'nao_encontrado'
+        else:
+            if len(questao['opcoes'].keys()) != 4: 
+                saida['opcoes'] = 'tamanho_invalido' #5 
+            else:
+                for letra, valor in questao['opcoes'].items():
+                
+                    if letra in lista_letras:
+                        if valor.strip() == '':
+                            saida_opcoes[letra] = 'vazia' #7
+                            saida['opcoes'] = saida_opcoes
+                    else:
+                        saida_opcoes[letra] = 'chave_invalida_ou_nao_encontrada' #6
+                        saida['opcoes'] = saida_opcoes 
+
+        if 'correta' not in questao.keys(): #1
+            saida['correta'] = 'nao_encontrado'
+        else: 
+            if questao['correta'] not in lista_letras:
+                saida['correta'] = 'valor_errado' #8
+
+        return saida
+
+    lista_saida = []
+    for i in lista_questoes:
+        lista_saida.append(valida_questao(i))
+    return lista_saida
+
